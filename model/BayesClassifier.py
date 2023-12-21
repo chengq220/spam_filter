dcd2f4178df6af8f9b96aad702020b75eb64bd9c
Cheng Qiu <chengqiu220@gmail.com>
2023-12-21 18:24:32 -0500

    working bayes

--- model/BayesClassifier.py
+++ model/BayesClassifier.py
@@ -3,33 +3,61 @@ import numpy as np
 
 class NaiveBayes:
     def __init__(self, weight_loc, threshold=0.5):
-        self.weight = self.getWeight(weight_loc) #P(Spam | word)
-        self.spam_prob = 1 #Probability of spam
-        self.word_prob = 1 #The probability that that the word shows up
+        self.weight, self.word_prob, self.spam_prob= self.getWeight(weight_loc)
         self.threshold = threshold
 
+    """Initilialize the weights for the model"""
     def getWeight(self,address):
         weight = {}
-        df = pd.read_csv(address)
+        df = pd.read_csv(address + "/weight_pba.csv")
         key = df.iloc[:,0].to_numpy().squeeze()
         prob = df.iloc[:,1].to_numpy().squeeze()
         for i in range(key.shape[0]):
             weight[key[i]] = prob[i]
-        return weight
+
+        word_prob = {}
+        df = pd.read_csv(address + "/weight_pa.csv")
+        key = df.iloc[:,0].to_numpy().squeeze()
+        prob = df.iloc[:,1].to_numpy().squeeze()
+        for i in range(key.shape[0]):
+            word_prob[key[i]] = prob[i]
+
+        file = open(address + "/weight_pb.txt", "r")
+        spam_prob = float(file.read())
+
+        return weight, word_prob, spam_prob
 
 
     def predict(self,feature):
         probability = 1
         tokenize = feature.lower().split(' ')
+
+        itemProbNotSpam = 1
         for i in tokenize:
-            item_prob = (self.spam_prob * self.weight.get(i,1)) / self.word_prob.get(i,1)
+            #Bayes theorem
             #if the value does not exist in the weight, set it to 0.5 to show
             #that the model is unsure
-            probability = probability * item_prob
-        print(probability)
-        print(self.threshold/len(tokenize))
-        return probability >= self.threshold/len(tokenize)
+            if(self.word_prob.get(i,0) == 0):
+                itemProbSpam = 1
+            else:
+                itemProbSpam = (self.word_prob[i] * self.weight[i])
+                itemProbNotSpam = itemProbNotSpam * (1 -self.word_prob[i]) * (1-self.weight[i])
 
-model = NaiveBayes("../weight/weights.csv")
-phrase = "You win 5000 dollars. Claim it by friday"
-model.predict(phrase)
+
+            #THe probability is the produt of all the probability of the word given
+            #that it is a scam
+            #
+            probability = probability * itemProbSpam
+        normalize = 0
+        for i in range(2):
+            if(i%2 == 0):
+                normalize = normalize + self.spam_prob * probability
+            else:
+                normalize = normalize + (1 - self.spam_prob) * itemProbNotSpam
+        probability = probability/normalize
+        return probability >= self.threshold
+
+model = NaiveBayes("../weights")
+phrase = "You just win 5000 dollars. Claim it now"
+prediction = model.predict(phrase)
+print(prediction)
--- weights/weightProcessor.py
+++ weights/weightProcessor.py
@@ -9,6 +9,8 @@ class preprocessor:
         self.features = None
         self.spam_occurence = {}
         self.word_occurence = {}
+        self.spam_total = 0
+        self.total = 0
 
     """Read csv file"""
     def read_csv(self, address):
@@ -26,12 +28,17 @@ class preprocessor:
         self.df = self.df.iloc[:,:2]
         self.labels = pd.get_dummies(self.df.iloc[:,:1]).iloc[:,:1].to_numpy().squeeze()
         features = self.df.iloc[:,1:2].to_numpy().squeeze()
-        for i in range(features.shape[0]):
+        self.total = features.shape[0]
+        for i in range(self.total):
             self.tokenizeCalc(features[i], self.labels[i])
+        print(self.spam_total)
+        self.spam_total = self.spam_total/self.total
 
     """Tokenize the feature and calculate its sample statistics"""
     def tokenizeCalc(self, feature, label):
         tokenize = feature.lower().split(' ')
+        if not label:
+            self.spam_total = self.spam_total + 1
         for i in tokenize:
             if(self.word_occurence.get(i, 0) == 0):
                 if(not label):
@@ -47,24 +54,32 @@ class preprocessor:
                 self.word_occurence[i] = self.word_occurence[i] + 1
 
     """Return the weights for the dataset"""
-    def getWeight(self):
-        weight = {}
+    def getWeights(self):
+        pba = {}
+        pa = {}
         for key in self.spam_occurence.keys():
-            weight[key] = self.spam_occurence[key]/self.word_occurence[key]
-        return weight
+            pba[key] = self.spam_occurence[key]/self.word_occurence[key]
+            pa[key] = self.word_occurence[key]/self.total
+        return pba, pa, self.spam_total
 
-    """Export the weight as """
-    def exportWeight(self):
-        weight = self.getWeight()
-        with open('weight.csv', 'w') as csvfile:
+    """Export the weight as csv and txt files"""
+    def exportWeights(self):
+        pba, pa, pb = self.getWeights()
+        with open('weight_pba.csv', 'w') as csvfile:
             writer = csv.writer(csvfile)
-            for key, value in weight.items():
+            for key, value in pba.items():
                writer.writerow([key, value])
-
+        with open('weight_pa.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            for key, value in pa.items():
+               writer.writerow([key, value])
+        with open('weight_pb.txt', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow([pb])
 
 
 
 p = preprocessor()
-p.read_csv("dataset/spam.csv")
+p.read_csv("../dataset/spam.csv")
 p.format()
-p.exportWeight()
+p.exportWeights()
