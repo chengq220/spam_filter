4fabfa455fefd6edd3775bf910deb232943dff87
Cheng Qiu <chengqiu220@gmail.com>
2023-12-25 21:48:46 -0500

    front-end work

--- naive-bayes-app/src/App.js
+++ naive-bayes-app/src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { API_URL } from "./environment/index";
 
 function App() {
   const [spamState, setSpamState]  = useState("Enter something in the text below to check if it is a spam or not!")
 
-  function myFunc(message){
-    console.log("function is getting called")
+  function predict(message){
+    // console.log("function is getting called")
     if(document.getElementById('message').value.trim() === "") {
       alert("Input should not be empty");
     }else{
+      setTimeout(ProgressBar(), 1000);
       axios.post(API_URL, message).then(response => {
         // console.log(response.data)
         if(response.data === 1){
-          setSpamState("The message is likely a spam message")
+          var bodyElement = document.querySelector('body');
+          bodyElement.style.backgroundColor = "red";
+          setSpamState("The message is likely a spam message");
+          setTimeout(() => {bodyElement.style.backgroundColor = "#ffffff"}, 1500);
         }else{
+          var bodyElement = document.querySelector('body');
+          bodyElement.style.backgroundColor = "green";
           setSpamState("The message is unlikely a spam message");
+          setTimeout(() => {bodyElement.style.backgroundColor = "#ffffff"}, 1500);
         }
+        document.querySelector('#message').value = "Enter text here";
       }).catch(error => {
         // Handle errors if the request fails
         console.error('Error sending data to backend:', error);
@@ -26,16 +34,44 @@ function App() {
     console.log("finish executing")
   }
 
+  function ProgressBar(){
+    var bar = document.querySelector(".progressBar");
+
+    var width = 0;
+    var increment = 1; // Adjust the increment value based on your preference
+    var intervalTime = 10; // Interval time in milliseconds
+
+    var id = setInterval(() => {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width += increment;
+        bar.style.width = width + '%';
+      }
+    }, intervalTime);
+  }
+
+  function clear(){
+    document.querySelector(".progressBar").style.width = "0%";
+    document.querySelector('#message').value = "Enter text here";
+  }
+
   return (
     <div className="App">
+      <div className="progressBar"></div>
       <div className="container">
-        <div className="classification">
-          <p>{spamState}</p>
-        </div>
-        <div className="form_body">
-          <input type="text" id="message" name="message" placeholder="Enter the text here"></input>
-          <div id="submit" onClick= {() => myFunc(document.getElementById('message').value)}>Send</div>
-        </div>
+        <div className="mainBody">
+          <div className="classification">
+            <p>{spamState}</p>
+          </div>
+          <div className="form_body">
+            <textarea  type="text" id="message" name="message" placeholder="Enter text here"></textarea >
+            <div className = "buttonContainer">
+              <div id="submit" onClick= {() => predict(document.getElementById('message').value)}>Verify</div>
+              <div id="clear" onClick = {() => clear()}>Clear</div>
+            </div>
+          </div>
+          </div>
       </div>
     </div>
   );
